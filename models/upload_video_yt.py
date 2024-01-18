b4e3585aa8bdf764cc2849435640b84755d08114
Dung Tien <nguyentiendung12c701@gmail.com>
2024-01-18 13:38:55 +0700

    upload by file youtube

--- models/upload_video_yt.py
+++ models/upload_video_yt.py
@@ -1,5 +1,8 @@
-import time
+import os
+import pathlib
 import random
+import subprocess
+import base64
 
 from odoo import fields, models, api
 from odoo.exceptions import ValidationError
@@ -25,8 +28,25 @@ class UploadVideoYt(models.Model):
     tags = fields.Char(string='Tags', required=True)
     publishAt = fields.Datetime(string='publishAt', required=True)
     isPublish = fields.Boolean(string='Is Publish', default=False, readonly=True)
-    path = fields.Char(string='Path', required=True)
     channel = fields.Many2one('google.access.token', string='Youtube Channel', required=True)
+    video_file = fields.Binary('Video file')
+
+    @staticmethod
+    def overwrite_input_file(path, data):
+        file = open(path, 'wb')
+        file.write((base64.b64decode(data)))
+        file.close()
+
+    def get_mp4_file(self):
+        input_file = f'{pathlib.Path(__file__).parent.parent}/static/video/video.bin'
+        self.overwrite_input_file(input_file, self.video_file)
+
+        output_file = f'{pathlib.Path(__file__).parent.parent}/static/video/{random.randint(10000, 99999)}.mp4'
+
+        command = f"ffmpeg -i {input_file} {output_file}"
+        subprocess.call(command, shell=True)
+
+        return output_file
 
     def upload(self):
         if self.isPublish:
@@ -53,13 +73,17 @@ class UploadVideoYt(models.Model):
 
             youtube = self.build_youtube_obj(self.channel, client_id, client_secret)
 
+            file_path = self.get_mp4_file()
+            print(f'Created file at {file_path}')
+
             res = youtube.videos().insert(
                 part='snippet,status',
                 body=body,
-                media_body=MediaFileUpload(self.path, chunksize=-1, resumable=True))
+                media_body=MediaFileUpload(file_path, chunksize=-1, resumable=True))
 
             print('Uploading file...')
             status, response = res.next_chunk()
+            print(" ----- response ----- ".upper())
             print(response)
             if response is not None:
                 if 'id' in response:
@@ -70,6 +94,9 @@ class UploadVideoYt(models.Model):
                     result = f'The upload failed with an unexpected response: {response}'
             else:
                 result = 'There is no response'
+
+            os.remove(file_path)
+            print(f'Deleted file at {file_path}')
 
             if result == 'Success':
                 self.isPublish = True
